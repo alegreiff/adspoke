324c1f1db8f344f27334cc1be50f5e6cab19cfda
Jaime de Greiff <alegreiff@gmail.com>
2024-09-21 17:37:07 -0500

    imagen corregida

--- src/components/pokemones/FavoritePokemonCard.tsx
+++ src/components/pokemones/FavoritePokemonCard.tsx
@@ -1,21 +1,43 @@
 import type { FavoritePokemon } from "@interfaces/favorite-pokemons";
-import { createSignal, type Component } from "solid-js";
+import { createSignal, Show, type Component } from "solid-js";
 
 interface Props {
   pokemon: FavoritePokemon;
 }
 
 export const FavoritePokemonCard: Component<Props> = ({ pokemon }) => {
-  const [visible, isVisible] = createSignal(true);
+  const [isVisible, setIsVisible] = createSignal(true);
   const imageSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`;
+
+  const deleteFavorite = () => {
+    const favorites = JSON.parse(
+      localStorage.getItem("favorites") ?? "[]"
+    ) as FavoritePokemon[];
+    const newFavorites = favorites.filter((p) => p.id !== pokemon.id);
+    localStorage.setItem("favorites", JSON.stringify(newFavorites));
+    setIsVisible(false);
+  };
   return (
     <>
-      <div class="flex flex-col justify-center items-center">
-        <a href={`/pokemon/${pokemon.name}`}>
-          #{pokemon.id} {pokemon.name}
-          <img src={imageSrc} alt="" />
-        </a>
-      </div>
+      <Show when={isVisible()}>
+        <div class="flex flex-col justify-center items-center">
+          <a href={`/pokemons/${pokemon.name}`}>
+            <img
+              style={`view-transition-name: ${pokemon.name}-image`}
+              src={imageSrc}
+              alt={pokemon.name}
+              width="96"
+              height="96"
+            />
+            <p class="capitalize">
+              #{pokemon.id} {pokemon.name}
+            </p>
+          </a>
+          <button onClick={deleteFavorite} class="text-red-400 ">
+            Borrar
+          </button>
+        </div>
+      </Show>
     </>
   );
 };
--- src/components/pokemones/FavoritePokemons.tsx
+++ src/components/pokemones/FavoritePokemons.tsx
@@ -15,11 +15,7 @@ export const FavoritePokemons = () => {
   return (
     <div class="grid grid-cols-2 sm:grid-cols-4">
       <For each={pokemons()}>
-        {(pokemon) => (
-          <>
-            <FavoritePokemonCard pokemon={pokemon} />
-          </>
-        )}
+        {(pokemon) => <FavoritePokemonCard pokemon={pokemon} />}
       </For>
     </div>
   );
